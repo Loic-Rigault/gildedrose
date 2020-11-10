a3124647d4eb4b3405d079050f11205c9244b060
Loïc Rigault <56129343+Loic-Rigault@users.noreply.github.com>
2020-11-10 13:17:21 +0100

    Update GildedRose.java
    Correction après avoir utiliser le Gradlew check

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,57 +1,59 @@
 package com.gildedrose;
 
 class GildedRose {
+
+  private static final int MAX_QUALITY = 50;
+  private static final int TIME_BACKSTAGE_FIVE = 5;
+  private static final int TIME_BACKSTAGE_TEN = 10;
+
   Item[] items;
 
   public GildedRose(Item[] items) {
     this.items = items;
-
   }
 
 
 
   public void updateQuality() {
 
-    for(Item item : items){
-      /*Si on a sulfuras alors on change rien car il ne perd pas en qualité */
-      if(item.name == "Sulfuras, Hand of Ragnaros"){
-	return;
-	}
-  		/* Le switch permet une meilleur compréhension en fonction des noms d'items, de plus nous n'avons pas de répétition */
+    for (Item item : items) {
+      if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        return;
+      }
+  /* Le switch permet une meilleur compréhension en fonction des noms d'items*/
       switch (item.name) {
         case "Aged Brie":
-          updateBrie(item); // Je propose de remplacer chaque action sur l'item par une méthode simple pour éviter la redondance
-        break;
+          updateBrie(item);
+/* Il faut remplacer chaque action sur l'item par une méthode simple pour éviter la redondance */
+          break;
         case "Backstage passes to a TAFKAL80ETC concert":
           updateBackstage(item);
-        break;
+          break;
         default:
           reduceQuality(item);
           if (item.sellIn <= 0) {
             reduceQuality(item);
-          } 
-        break;
+          }
+          break;
       }
-    
     /* Comme le sellin diminue à chaque fois alors on le fait pour tous */
-    item.sellIn = item.sellIn - 1;
-    
+      item.sellIn = item.sellIn - 1;
     }
-  }  
+  }
 
   public void updateBrie(Item item) {
-  	addQuality(item);
+    addQuality(item);
     if (item.sellIn <= 0) {
       addQuality(item);
     }
   }
 
   public void updateBackstage(Item item) {
-  	addQuality(item);
-    if (item.sellIn <= 10) {
+    addQuality(item);
+    if (item.sellIn <= TIME_BACKSTAGE_TEN) {
       addQuality(item);
     }
-    if (item.sellIn <= 5) {
+    if (item.sellIn <= TIME_BACKSTAGE_FIVE) {
       addQuality(item);
     }
     if (item.sellIn <= 0) {
@@ -60,13 +62,12 @@ class GildedRose {
   }
 
 
-  /* 
-   	 Ces methodes vont permettre de reduire la redondance des conditions sur la qualité 
-     tout en respectant les prérequis (Si c'est supérieur à 50 ou si c'est négatif)
-  */
+/* Ces methodes vont permettre de reduire la redondance
+des conditions sur la qualité tout en respectant les prérequis.
+(Si c'est supérieur à 50 ou si c'est négatif) */
 
   public void addQuality(Item item) {
-    if (item.quality < 50) {
+    if (item.quality < MAX_QUALITY) {
       item.quality++;
     }
   }
